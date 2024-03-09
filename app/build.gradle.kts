5a45183c57f381b0e6b27c961390c5407a666ce7
Guigax <guilherme.fonseca.eu@gmail.com>
2024-03-09 19:16:39 -0300

    Adjusted so changes on views are not inside coroutines

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.guigax.loudscoreboard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
