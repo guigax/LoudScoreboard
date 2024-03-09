26a7fb2af0b18872cc2daee9efea3d48884423d8
Guigax <guilherme.fonseca.eu@gmail.com>
2024-03-09 19:23:43 -0300

    Adjusted version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.guigax.loudscoreboard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
