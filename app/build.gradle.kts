57dd30d1cbb6682cb73acb39de05d07b754c8099
Guigax <guilherme.fonseca.eu@gmail.com>
2024-03-09 19:27:51 -0300

    Updated app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.guigax.loudscoreboard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
