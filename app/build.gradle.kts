c791de780ad8d184f8a6a7f53e0cf1de5b52501a
Guigax <guilherme.fonseca.eu@gmail.com>
2024-08-23 07:58:38 -0300

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.guigax.loudscoreboard"
         minSdk = 29
         targetSdk = 34
-        versionCode = 9
-        versionName = "1.3.0"
+        versionCode = 10
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
